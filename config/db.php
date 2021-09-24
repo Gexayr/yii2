e1a511d21041da307027ef250c772e738eaf478f
gexayr <gexo7777@gmail.com>
2021-09-24 17:04:45 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=gexo777_yii2',
+    'username' => 'gexo777_yii2',
+    'password' => 'gexo777_yii2Q!@',
+    'charset' => 'utf8',
+];
